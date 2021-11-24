754bbde7cb4825a04b2b9b1d9b27af0d3ad36865
directx5 <73019195+directx5@users.noreply.github.com>
2021-11-24 20:18:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,50 +8,62 @@ array = [
 ]
 
 
-def mix(val):
+def mix(val: int):
+    """
+    Hataları engellemek için girilen değerin max ve min değerlerin arasında kalmasını sağlar.
+    """
     return max(0, min(len(array) - 1, val))
 
 
-def ones(r, c):
-    top = mix(r - 1), c
-    bot = mix(r + 1), c
-    left = r, mix(c - 1)
-    right = r, mix(c + 1)
+def explore(row, column):
+    """
+    Verilen karenin dört bir tarafındaki 1'leri bulur.
+    """
+    top = mix(row - 1), column
+    bot = mix(row + 1), column
+    left = row, mix(column - 1)
+    right = row, mix(column + 1)
 
     return [(r, c) for r, c in (top, bot, left, right) if array[r][c] == 1]
 
 
-# kenarlardaki birler
-def is_edge_one(row, column):
-    return (row == 0 or row == len(array) - 1 or column == 0 or column == len(array[0]) - 1) and array[row][column] == 1
+def edge(row: int, column: int):
+    """
+    Kenarda ve bir mi?
+    """
+    return (row == 0 or row == len(array) - 1 or column == 0 or column == len(array) - 1) and array[row][column] == 1
 
 
-# kenarlardaki birlere bağlı olan birler
-def next_to_edge_one(row, column):
-    points = ones(row, column)
-    while not any(is_edge_one(r, c) for r, c in points):
-        points = [y for r, c in points for y in ones(r, c)]
-        if len(points) - 1 < 1:  # şu anki konuma gelirken kullandığımız konumu siliyorum
+def connected(row: int, column: int):
+    """
+    Kenardaki herhangi bir 1'e bağlı mı?
+    """
+    points = explore(row, column)
+    while not any(edge(r, c) for r, c in points):
+        points = [sub_arr for r, c in points for sub_arr in explore(r, c)]
+        if len(points) - 1 < 1:  # Şu anki konumda explore'u kullandığımızda listeye eski konum da dahil olduğu için 1 çıkarıyorum.
             return False
     return True
 
 
 def main():
-    for y in range(len(array)):
-        for x in range(len(array[0])):
-            if not next_to_edge_one(y, x) and not is_edge_one(y, x) and array[y][x] == 1:
-                array[y][x] = 0
+    for row in range(len(array)):
+        for column in range(len(array)):
+            if not connected(row, column) and not edge(row, column) and array[row][column] == 1:
+                array[row][column] = 0
 
-    for i in array:
-        print(i)
+    for row in array:
+        print(row)
 
 
 if __name__ == '__main__':
     main()
 
-# [1, 0, 0, 0, 0, 0]
-# [0, 0, 0, 1, 1, 1]
-# [0, 0, 0, 0, 1, 0]
-# [1, 1, 0, 0, 1, 0]
-# [1, 0, 0, 0, 0, 0]
-# [1, 0, 0, 0, 0, 1]
+'''
+[1, 0, 0, 0, 0, 0]
+[0, 0, 0, 1, 1, 1]
+[0, 0, 0, 0, 1, 0]
+[1, 1, 0, 0, 1, 0]
+[1, 0, 0, 0, 0, 0]
+[1, 0, 0, 0, 0, 1]
+'''
